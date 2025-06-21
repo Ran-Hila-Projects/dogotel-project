50917ae6224d068707006f4728e1bee1707d42d0
Ran Meshulam <ranmes16@gmail.com>
2025-06-21 13:46:50 +0300

    more changes

--- dogotel-frontend/src/config.js
+++ dogotel-frontend/src/config.js
@@ -1,15 +1,12 @@
 const CONFIG = {
-  API_URL: "https://3np1pkm7l7.execute-api.us-east-1.amazonaws.com/Prod/",
-  COGNITO_USER_POOL_ID: "us-east-1_KDxstqsvs",
-  COGNITO_CLIENT_ID: "55ibmlj595539o62iqi675gt62",
+  API_URL: "__API_URL__",
+  COGNITO_USER_POOL_ID: "__COGNITO_USER_POOL_ID__",
+  COGNITO_CLIENT_ID: "__COGNITO_CLIENT_ID__",
   REGION: "us-east-1",
-  S3_BUCKET_NAME: "dogotel-images-652867883117",
-  IMAGE_BASE_URL:
-    "https://dogotel-images-652867883117.s3.amazonaws.com/images/",
-  COGNITO_HOSTED_UI_URL:
-    "https://dogotel-auth-1750501652.auth.us-east-1.amazoncognito.com/",
-  REDIRECT_URL:
-    "http://dogotel-website-652867883117.s3-website-us-east-1.amazonaws.com/",
+  S3_BUCKET_NAME: "__S3_BUCKET_NAME__",
+  IMAGE_BASE_URL: "__IMAGE_BASE_URL__",
+  COGNITO_HOSTED_UI_URL: "__COGNITO_HOSTED_UI_URL__",
+  REDIRECT_URL: "__REDIRECT_URL__"
 };
 
 export default CONFIG;
